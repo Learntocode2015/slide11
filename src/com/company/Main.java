c974efab46d933cfa6d63b0e5b9c5b6c4283422c
chris.lloyd <learntocode2015@outlook.com>
2021-09-09 10:41:26 +0100

    Added comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 public class Main {
     //finding if a number is true or false
     public static void main(String[] args) {
-         System.out.println(TrueOrFalseNum(UserNumberChoice()));
+        while (true) {
+            try {
+                /*
+                Integer.parseInt converts user input to an integer and throws
+                and error if it's not an integer.
+                 */
+                System.out.println(TrueOrFalseNum(Integer.parseInt(UserNumberChoice())));
+            }
+            catch (Exception e) {
+                break;
+            }
+
+        }
     }
 
-    public static int UserNumberChoice(){
+    public static String UserNumberChoice() {
         System.out.println("please enter a number");
         Scanner input = new Scanner(System.in);
-        return input.nextInt();
+        return input.next();
+
     }
+
     public static boolean TrueOrFalseNum(int a) {
         return (a % 2 == 0);
     }
-
-
-
 
 }
