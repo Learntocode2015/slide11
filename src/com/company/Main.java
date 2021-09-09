b462af6139bd8960785f71ba184f34dab220017f
chris.lloyd <learntocode2015@outlook.com>
2021-09-09 10:12:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+         System.out.println(TrueOrFalseNum(UserNumberChoice()));
+    }
+
+    public static int UserNumberChoice(){
+        System.out.println("please enter a number");
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+    public static boolean TrueOrFalseNum(int a) {
+        return (a % 2 == 0);
+    }
+
+
+
+
+}
